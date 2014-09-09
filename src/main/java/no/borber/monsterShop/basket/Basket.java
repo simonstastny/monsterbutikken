e0ee0c4a10d2f103d1538c3e19379ec7b4c6f1c3
Ricardo Costa <costa.rag@gmail.com>
2014-09-09 20:20:01 +0200

    Pair programming with Simon Statsny.

---
+++ src/main/java/no/borber/monsterShop/basket/Basket.java
@@ -0,0 +1,10 @@
+package no.borber.monsterShop.basket;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Basket {
+  
+  public final List<BasketItem> items = new ArrayList<>();
+  
+}
